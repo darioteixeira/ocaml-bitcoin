b61d60fae68aa5c587ddf5b6dc957434862593a3
darioteixeira <darioteixeira@yahoo.com>
2015-02-18 16:55:02 +0000

    Port to OCamlnet 4

--- src/bitcoin_ocamlnet.ml
+++ src/bitcoin_ocamlnet.ml
@@ -27,8 +27,8 @@ struct
 
 	let post_string ~headers ~inet_addr ~host ~port ~uri request =
 		let dst = "http://" ^ (Unix.string_of_inet_addr inet_addr) ^ ":" ^ (string_of_int port) ^ uri in
-		let pipeline = new Http_client.pipeline in
-		let request = new Http_client.post_raw dst request in
+		let pipeline = new Nethttp_client.pipeline in
+		let request = new Nethttp_client.post_raw dst request in
 		let () = List.iter (fun (k, v) -> request#set_req_header k v) headers in
 		let () = pipeline#add request in
 		let () = pipeline#run () in
