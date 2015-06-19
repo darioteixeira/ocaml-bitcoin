ac8aed4e0dacc1b7279e88917dfe855fbb97ff77
darioteixeira <darioteixeira@yahoo.com>
2015-06-19 12:04:31 +0100

    Retabbing: replace TAB characters with 4 spaces

--- src/bitcoin_cohttp.ml
+++ src/bitcoin_cohttp.ml
@@ -1,22 +1,22 @@
 (********************************************************************************)
-(*	Bitcoin_cohttp.ml
-	Copyright (c) 2013 Vincent Bernardoff <vb@luminar.eu.org>
+(*  Bitcoin_cohttp.ml
+    Copyright (c) 2013 Vincent Bernardoff <vb@luminar.eu.org>
 *)
 (********************************************************************************)
 
-(**	Offers an implementation of a {!Bitcoin.HTTPCLIENT} using Cohttp's
-	[Cohttp_lwt_unix.Client].
+(** Offers an implementation of a {!Bitcoin.HTTPCLIENT} using Cohttp's
+    [Cohttp_lwt_unix.Client].
 *)
 
 (********************************************************************************)
-(**	{1 Exceptions}								*)
+(** {1 Exceptions}                                                              *)
 (********************************************************************************)
 
 exception No_response
 
 
 (********************************************************************************)
-(**	{1 Private modules}							*)
+(** {1 Private modules}                                                         *)
 (********************************************************************************)
 
 module C = Cohttp
@@ -25,22 +25,23 @@ module CB = Cohttp_lwt_body
 
 
 (********************************************************************************)
-(**	{1 Public modules}							*)
+(** {1 Public modules}                                                          *)
 (********************************************************************************)
 
 module Httpclient: Bitcoin.HTTPCLIENT with type 'a Monad.t = 'a Lwt.t =
 struct
-  module Monad = Lwt
+    module Monad = Lwt
 
-  let (>>=) = Lwt.bind
+    let (>>=) = Lwt.bind
 
-  let post_string ~headers ~inet_addr ~host ~port ~uri request =
-    let headers = C.Header.of_list headers in
-    let uri = Uri.make ~scheme:"http" ~host ~port ~path:uri () in
-    Lwt.try_bind
-      (fun () -> CU.Client.call ~chunked:false ~headers ~body:(CB.of_string request) `POST uri)
-      (fun (_, b) -> CB.to_string b)
-      (fun exn ->
-         Lwt_io.printf "Cohttp_lwt_unix.Client.call: caught %s\n" (Printexc.to_string exn) >>= fun () ->
-         Lwt.fail exn)
+    let post_string ~headers ~inet_addr ~host ~port ~uri request =
+        let headers = C.Header.of_list headers in
+        let uri = Uri.make ~scheme:"http" ~host ~port ~path:uri () in
+        Lwt.try_bind
+            (fun () -> CU.Client.call ~chunked:false ~headers ~body:(CB.of_string request) `POST uri)
+            (fun (_, b) -> CB.to_string b)
+            (fun exn ->
+                Lwt_io.printf "Cohttp_lwt_unix.Client.call: caught %s\n" (Printexc.to_string exn) >>= fun () ->
+                Lwt.fail exn)
 end
+
--- src/bitcoin_ocurl.ml
+++ src/bitcoin_ocurl.ml
@@ -1,47 +1,47 @@
 (********************************************************************************)
-(*	Bitcoin_ocurl.ml
-	Copyright (c) 2013 Vincent Bernardoff <vb@luminar.eu.org>
+(*  Bitcoin_ocurl.ml
+    Copyright (c) 2013 Vincent Bernardoff <vb@luminar.eu.org>
 *)
 (********************************************************************************)
 
-(**	Offers an implementation of a {!Bitcoin.HTTPCLIENT} using OCurl's
-	[Http_client].
+(** Offers an implementation of a {!Bitcoin.HTTPCLIENT} using OCurl's
+    [Http_client].
 *)
 
 
 (********************************************************************************)
-(**	{1 Public modules}							*)
+(** {1 Public modules}                                                          *)
 (********************************************************************************)
 
 module Httpclient: Bitcoin.HTTPCLIENT with type 'a Monad.t = 'a =
 struct
-  module Monad =
-  struct
-    type 'a t = 'a
+    module Monad =
+    struct
+        type 'a t = 'a
 
-    let return x = x
-    let fail x = raise x
-    let bind t f = f t
-    let catch t f = try t () with exc -> f exc
-  end
+        let return x = x
+        let fail x = raise x
+        let bind t f = f t
+        let catch t f = try t () with exc -> f exc
+    end
 
-  let buf = Buffer.create 100
+    let buf = Buffer.create 100
 
-  let post_string ~headers ~inet_addr ~host ~port ~uri request =
-    let open Curl in
-    let h = new handle in
-    let dst = "http://" ^ host ^ ":" ^ (string_of_int port) ^ uri in
-    try
-      h#set_post true;
-      h#set_postfields request;
-      h#set_postfieldsize (String.length request);
-      h#set_url dst;
-      h#set_writefunction (fun s -> Buffer.add_string buf s; String.length s);
-      h#set_httpheader (List.map (fun (k,v) -> k ^ ": " ^ v) headers);
-      h#perform;
-      h#cleanup;
-      let contents = Buffer.contents buf in Buffer.reset buf; contents
-    with CurlException (_, i, s) as exn ->
-      Printf.eprintf "EXN: %d %s\n%!" i s; raise exn
+    let post_string ~headers ~inet_addr ~host ~port ~uri request =
+        let open Curl in
+        let h = new handle in
+        let dst = "http://" ^ host ^ ":" ^ (string_of_int port) ^ uri in
+        try
+            h#set_post true;
+            h#set_postfields request;
+            h#set_postfieldsize (String.length request);
+            h#set_url dst;
+            h#set_writefunction (fun s -> Buffer.add_string buf s; String.length s);
+            h#set_httpheader (List.map (fun (k,v) -> k ^ ": " ^ v) headers);
+            h#perform;
+            h#cleanup;
+            let contents = Buffer.contents buf in Buffer.reset buf; contents
+        with CurlException (_, i, s) as exn ->
+            Printf.eprintf "EXN: %d %s\n%!" i s; raise exn
 end
 
