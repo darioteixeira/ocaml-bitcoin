f591f17ed5d1e442b06ef1468a42a6854919c545
Michael Bacarella <m@bacarella.com>
2023-01-23 07:50:34 -0800

    Take credit/blame for the async module ;-)

--- lib_cohttp_async/bitcoin_cohttp_async.ml
+++ lib_cohttp_async/bitcoin_cohttp_async.ml
@@ -1,5 +1,6 @@
 (*  Bitcoin_cohttp_async.ml
     Copyright (c) 2021 Michael Bacarella <m@bacarella.com> *)
+open! Core
 open! Async
 
 (** Offers an implementation of a {!Bitcoin.HTTPCLIENT} using Cohttp's [Cohttp_lwt_async.Client].
@@ -23,7 +24,8 @@ module CB = Cohttp_async.Body
 (** {1 Public modules}                                                          *)
 (********************************************************************************)
 
-module Httpclient : Bitcoin.HTTPCLIENT with type 'a Monad.t = ('a, exn) Result.t Deferred.t = struct
+module Httpclient :
+  Bitcoin.HTTPCLIENT with type 'a Monad.t = ('a, exn) Result.t Deferred.t = struct
   (* Unlike Lwt promises, Async promises don't attach errors to it.  So,
      we wrap Async promises around a Result.t to satisfy the interface. *)
   module Monad = struct
@@ -48,10 +50,16 @@ module Httpclient : Bitcoin.HTTPCLIENT with type 'a Monad.t = ('a, exn) Result.t
   end
 
   let post_string ~headers ~inet_addr:_ ~host ~port ~uri request =
-    let headers = C.Header.of_list headers in
+    (* Leaving out the 'connection: close' here causes lingering old connections to pile up. *)
+    let headers = C.Header.of_list (("connection", "close") :: headers) in
     let uri = Uri.make ~scheme:"http" ~host ~port ~path:uri () in
     Monitor.try_with (fun () ->
-        Cohttp_async.Client.call ~chunked:false ~headers ~body:(CB.of_string request) `POST uri)
+        Cohttp_async.Client.call
+          ~chunked:false
+          ~headers
+          ~body:(CB.of_string request)
+          `POST
+          uri)
     >>= fun res ->
     match res with
     | Error exn -> Monad.fail exn
